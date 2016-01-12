1770901cda6411f2355979b211481f168ed45cd0
A. Pic <dev@apic.name>
2016-01-12 16:57:27 +0100

    Zeichensatz von UTF-16 zu UTF-8 rekodieren.

--- Betcoin2Pokerstars.zsh
+++ Betcoin2Pokerstars.zsh
@@ -5,6 +5,22 @@ set -e
 # Kommandos vor dem Exekutieren anzeigen:
 set -x
 
-
 # Sicherstellen dass Quelltextdatei vorhanden ist:
 test -e Betcoin.txt
+
+# Erkennen ob der Zeichensatz passt
+Charset=`file Betcoin.txt`
+Wanted_Charset="Betcoin.txt: "
+Wanted_Charset+="Little-endian UTF-16 Unicode text, "
+Wanted_Charset+="with CRLF line terminators"
+test "$Charset" = "$Wanted_Charset"
+
+# Zeichensatz Rekodieren von UTF16 nach UTF8
+recode utf16..utf8 Betcoin.txt
+
+# Erkennen ob das Rekodieren zum erwünschsten Zeichensatz führte
+Charset=`file Betcoin.txt`
+Wanted_Charset="Betcoin.txt: "
+Wanted_Charset+="ASCII text, "
+Wanted_Charset+="with CRLF line terminators"
+test "$Charset" = "$Wanted_Charset"
