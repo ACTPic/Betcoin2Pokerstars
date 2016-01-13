7b4e68deaaefc17e31a3517cbf623dff2806951b
A. Pic <dev@apic.name>
2016-01-13 13:08:04 +0100

    Zerlegeskript aufrufen.

--- Betcoin2Pokerstars.zsh
+++ Betcoin2Pokerstars.zsh
@@ -24,3 +24,6 @@ Wanted_Charset="Betcoin.txt: "
 Wanted_Charset+="ASCII text, "
 Wanted_Charset+="with CRLF line terminators"
 test "$Charset" = "$Wanted_Charset"
+
+# Awk-Skript die Spieledaten zerlegen lassen
+./Betcoin_zerlegen.gawk Betcoin.txt
