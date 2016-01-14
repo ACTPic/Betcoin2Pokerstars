b9a06f297ca092aeea0a91b7f0052399cee6045f
A. Pic <dev@apic.name>
2016-01-14 13:54:53 +0100

    Spiel-ID einlesen.

--- Betcoin_zerlegen.pl
+++ Betcoin_zerlegen.pl
@@ -4,14 +4,14 @@ use strict;
 use warnings;
 use DateTime;
 
+my ($Startzeile, $Zeitstempel) = undef;
+
 while ($_ = <STDIN>) {
         s/\r?\n//;
         if(not length) {
                 print "___Separator___\n";
                 next;
         }
-
-        my ($Startzeile, $Zeitstempel) = undef;
 
         if (/^Game started at:/) {
                 $Startzeile = $_;
@@ -29,4 +29,16 @@ while ($_ = <STDIN>) {
                 $Zeitstempel = $dt->epoch;
                 print("Zeitstempel: @" . $Zeitstempel . "\n");
         }
+
+        if (/^Game ID: \d+/) {
+                $_ =~ m|^Game ID: (\d+)|;
+
+                if(not $Startzeile or not $Zeitstempel) {
+                        die "ID ohne Startzeile/Zeitstempel eingelesen.";
+                }
+
+                my $ID = $1;
+                print("ID: " . $ID);
+        }
+
 }
