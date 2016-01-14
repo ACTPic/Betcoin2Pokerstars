93b2ca60cdee9af1c1afa0a8d5c0608f1bd7b1c9
A. Pic <dev@apic.name>
2016-01-14 14:28:37 +0100

    Verzeichnis anlegen und Daten in Datei schreiben.

--- Betcoin_zerlegen.pl
+++ Betcoin_zerlegen.pl
@@ -5,15 +5,20 @@ use warnings;
 use DateTime;
 
 my ($Startzeile, $Zeitstempel) = undef;
+my $Handle = undef;
 
 while ($_ = <STDIN>) {
         s/\r?\n//;
         if(not length) {
                 print "___Separator___\n";
+                if($Handle) {
+                        close $Handle;
+                        $Handle = undef;
+                }
                 next;
         }
 
-        if (/^Game started at:/) {
+        if(/^Game started at:/) {
                 $Startzeile = $_;
                 $_ =~ m|^Game.+at: (\d+)/(\d+)/(\d+) (\d+):(\d+):(\d+)|;
 
@@ -28,9 +33,7 @@ while ($_ = <STDIN>) {
 
                 $Zeitstempel = $dt->epoch;
                 print("Zeitstempel: @" . $Zeitstempel . "\n");
-        }
-
-        if (/^Game ID: \d+/) {
+        } elsif(/^Game ID: \d+/) {
                 $_ =~ m|^Game ID: (\d+)|;
 
                 if(not $Startzeile or not $Zeitstempel) {
@@ -39,6 +42,16 @@ while ($_ = <STDIN>) {
 
                 my $ID = $1;
                 print("ID: " . $ID . "\n");
-        }
 
+                my $Dir = "Pokerspiel_@" . $Zeitstempel . "_" . $ID;
+
+                mkdir $Dir or die "Verzeichnis für ID " . $ID . " besetzt.";
+                die $Dir . "/In.txt besetzt." if -e $Dir . "/In.txt";
+                open ($Handle, '>>', $Dir . "/In.txt") or die;
+                print $Handle $Startzeile . "\n" . $_;
+        } else {
+                print $Handle $_ . "\n" if $Handle;
+        }
 }
+
+close $Handle if $Handle;
