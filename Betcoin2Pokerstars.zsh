e45662d5277301bdd15efe17912e99ff284004fd
A. Pic <dev@apic.name>
2016-03-10 18:07:29 +0100

    Fehlermeldung bei unpassendem Zeichensatz und Fehlerlogging pro konvertierter Einzeldatei.

--- Betcoin2Pokerstars.zsh
+++ Betcoin2Pokerstars.zsh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-# Bei Fehler Script stoppen:
-set -e
-# Kommandos vor dem Exekutieren anzeigen:
-set -x
-
 cd in
 for i in HH*.txt; do
     o=${i/-G/}
@@ -21,19 +16,25 @@ for i in HH*.txt; do
         Wanted_Charset="../Betcoin.txt: "
         Wanted_Charset+="Little-endian UTF-16 Unicode text, "
         Wanted_Charset+="with CRLF line terminators"
-        test "$Charset" = "$Wanted_Charset"
 
-        # Zeichensatz Rekodieren von UTF16 nach UTF8
-        recode utf16..utf8 ../Betcoin.txt
+        if test "$Charset" = "$Wanted_Charset"; then
+            # Zeichensatz Rekodieren von UTF16 nach UTF8
+            recode utf16..utf8 ../Betcoin.txt
 
-        # Erkennen ob das Rekodieren zum erwünschsten Zeichensatz führte
-        Charset=`file ../Betcoin.txt`
-        Wanted_Charset="../Betcoin.txt: "
-        Wanted_Charset+="ASCII text, "
-        Wanted_Charset+="with CRLF line terminators"
-        test "$Charset" = "$Wanted_Charset"
-
-        # Perl-Skript die Spieledaten konvertieren lassen
-        ../B2P.pl < ../Betcoin.txt > ../out/$out
+            # Erkennen ob das Rekodieren zum erwünschsten Zeichensatz führte
+            Charset=`file ../Betcoin.txt`
+            Wanted_Charset="../Betcoin.txt: "
+            Wanted_Charset+="ASCII text, "
+            Wanted_Charset+="with CRLF line terminators"
+            if test "$Charset" = "$Wanted_Charset"; then
+                # Perl-Skript die Spieledaten konvertieren lassen
+                ../B2P.pl < ../Betcoin.txt > ../out/$out 2>../out/FEHLER_$out
+            else
+                echo "Rekodieren ergab nicht den gewünschten Zeichensatz." 1>&2
+            fi
+        else
+            echo "Zeichensatz ist '$Charset' statt '$Wanted_Charset'." 1>&2
+        fi
+        find ../out -empty -type f -delete
     fi
 done
